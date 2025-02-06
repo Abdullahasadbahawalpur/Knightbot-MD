51c18366faeab728572aca60c7dde390b0c6be69
Abdullahasadbahawalpur <164224515+Abdullahasadbahawalpur@users.noreply.github.com>
2025-02-06 22:40:33 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "DEVPLPED BY ABDULLAH ASAD",
+  botOwner: 'ABDULLAH ASAD', // Your name
+  ownerNumber: '923348544535.923246924762', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
